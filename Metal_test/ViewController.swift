b9a141ee9700ee13ed6fd651fe4da4304efbdd91
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2021-10-05 12:03:33 +0300

    first commit

--- Metal_test/ViewController.swift
+++ Metal_test/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import MetalKit
+
+enum Colors {
+    static let wenderlichGreen = MTLClearColor(red: 0.0,
+                                               green: 0.4,
+                                               blue: 0.21,
+                                               alpha: 1.0)
+}
 
 class ViewController: UIViewController {
 
+    var metalView: MTKView {
+        return view as! MTKView
+    }
+    
+    var device: MTLDevice!
+    var commandQueue: MTLCommandQueue!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        metalView.device = MTLCreateSystemDefaultDevice()
+        device = metalView.device
+        
+        metalView.clearColor = Colors.wenderlichGreen
+        
+        commandQueue = device.makeCommandQueue()
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: metalView.currentRenderPassDescriptor!)
+        
+        commandEncoder?.endEncoding()
+        commandBuffer?.present(metalView.currentDrawable!)
+        commandBuffer?.commit()
     }
-
-
 }
 
+extension ViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) { }
+    
+    func draw(in view: MTKView) {
+        
+    }
+}
