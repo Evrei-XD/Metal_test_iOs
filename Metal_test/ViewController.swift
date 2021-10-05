4b4e163d87e7bf9f76c3f1b1ca4f2b5a8de1a702
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2021-10-05 11:19:15 +0300

    Initial Commit

---
+++ Metal_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metal_test
+//
+//  Created by Motorica LLC on 05.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
